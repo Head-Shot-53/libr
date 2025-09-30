3ff83c9c026fa3a14b12b78b37192400d7e7f06c
Head-Shot-53 <denchikmashtack@gmail.com>
2025-09-30 21:14:24 +0300

    +

--- books/views.py
+++ books/views.py
@@ -16,7 +16,7 @@ def book_create(request):
 # READ (список книг)
 def book_list(request):
     books = Book.objects.all()
-    return render(request, "library/book_list.html", {"books": books})
+    return render(request, "library/books_list.html", {"books": books})
 
 # READ (деталі однієї книги)
 def book_detail(request, pk):
