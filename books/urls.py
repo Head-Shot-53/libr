fb67b6e70043bad8ec0f1116fd3a43b89c3056e6
Head-Shot-53 <denchikmashtack@gmail.com>
2025-09-30 21:25:29 +0300

    +

--- books/urls.py
+++ books/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.book_list, name="book_list"),
-    path("book/<int:pk>/", views.book_detail, name="book_detail"),
-    path("book/create/", views.book_create, name="book_create"),
-    path("book/<int:pk>/update/", views.book_update, name="book_update"),
-    path("book/<int:pk>/delete/", views.book_delete, name="book_delete"),
+    path('', views.books_list, name='book_list'),
+    path('book/detail/<int:id>/', views.book_detail, name='book_detail'),
+    path('book/create/', views.book_create, name='book_create'),
+    path('book/update/<int:id>/', views.book_update, name='book_update'),
+    path('book/delete/<int:id>/', views.book_delete, name='book_delete')
 ]
